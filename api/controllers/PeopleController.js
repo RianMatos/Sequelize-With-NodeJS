dfad1d8f669eee916e1aaf5b3dbb740aff5dbd1d
Rian Matos <rianmatos57@gmail.com>
2022-05-18 16:39:04 -0300

    created C and R from CRUD

--- api/controllers/PeopleController.js
+++ api/controllers/PeopleController.js
@@ -9,6 +9,26 @@ class PeopleController{
             return res.status(500).json(error.message);
         }
     }
+
+    static async getPersonById(req, res){
+        try {
+            const { id } = req.params;
+            const person = await database.Pessoas.findOne( {where: { id: Number(id) } } );
+            return res.status(200).json(person);
+        } catch (error) {
+            return res.status(404).send(error.message);
+        }
+    }
+
+    static async createPerson(req, res){
+        try {
+            const body = req.body;
+            const newPerson = await database.Pessoas.create(body);
+            return res.status(201).json(newPerson);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PeopleController
--- api/routes/peopleRoutes.js
+++ api/routes/peopleRoutes.js
@@ -3,5 +3,7 @@ const PeopleController = require('../controllers/PeopleController.js');
 const router = Router();
 
 router.get('/people', PeopleController.getPeople);
+router.get('/people/:id', PeopleController.getPersonById);
+router.post('/people', PeopleController.createPerson);
 
 module.exports = router
